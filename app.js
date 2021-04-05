97b85adeb199180a2c2462d8c18c3e601465208c
Cade <cadeburkett@Cades-MBP.attlocal.net>
2021-04-05 14:32:32 -0700

    followed instructions to get employee info

--- app.js
+++ app.js
@@ -33,3 +33,27 @@ const render = require("./lib/htmlRenderer");
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+
+const questions = [
+    {
+        type: "input",
+        message: "Enter employee name",
+        name: "empName",
+    },
+    {
+        type: "input",
+        message: "Enter employee email",
+        name: "empEmail",
+    },
+    {
+        type: "input",
+        message: "Enter employee id",
+        name: "empId",
+    },
+    {
+        type: "list",
+        message: "Select employee role",
+        name: "empRole",
+        choices: ["Intern", "Engineer", "Manager"]
+    },
+];
